b618a0ffdc4cbb9eaa6f090070f92b175c120f4c
Yuriy <YIEkimov@sberbank.ru>
2022-03-10 22:38:16 +0300

    Добавил команду получения рандомной фотографии Марса.

--- src/app.ts
+++ src/app.ts
@@ -3,8 +3,6 @@ const { Context, Markup, Telegraf } = require('telegraf')
 const { Update } = require('typegram')
 const fetch = require('node-fetch');
 
-const fileUrl = "https://api.nasa.gov/planetary/apod?api_key=" + process.env.API_KEY;
-
 type TBody = {
   title: string;
   explanation: string;
@@ -18,12 +16,13 @@ const bot = new Telegraf(token)
 
 bot.start((ctx: any) => {
   ctx.reply(
-    'Здравствуйте ' + ctx.from.first_name + '! \n\nНа данный момент бот находится в разработке, на данный момент реализовано получаение фото дня. В дальшейшем планируется реализовать получение фотографий Марса с марсохода Curiosity \n\n\n\nСписок команд: \n\n/photo_day - Фото дня'
+    'Здравствуйте ' + ctx.from.first_name + '! \n\nНа данный момент бот находится в разработке, на данный момент реализовано получаение фото дня. В дальшейшем планируется реализовать получение фотографий Марса с марсохода Curiosity \n\n\nСписок команд: \n\n/photo_day - Фото дня\n\n/mars - Случайная фотография из Марса'
   );
 });
 
-bot.command('/photo_day', async (ctx: any) => {
-  fetch(fileUrl)
+bot.command('/photo_day', (ctx: any) => {
+  const photoDayUrl = "https://api.nasa.gov/planetary/apod?api_key=" + process.env.API_KEY;
+  fetch(photoDayUrl)
   .then((response: any) => {
     if (response.ok) {
       response.json().then((data: any) => {
@@ -34,10 +33,10 @@ bot.command('/photo_day', async (ctx: any) => {
           hdurl: data.hdurl,
         }
 
-        const explanationShort = obj?.explanation.length <= 800 ? obj.explanation : ''
-
+        // const explanationShort = obj?.explanation.length <= 800 ? obj.explanation : ''
+        // \n\n${explanationShort}
         const opts = {
-          'caption': `*${obj.title}*\n\n${explanationShort} \n\n [Full photo](${data.hdurl})`,
+          'caption': `*${obj.title}* \n\n[Full photo](${data.hdurl})`,
           'parse_mode': 'markdown'
         };
 
@@ -47,8 +46,41 @@ bot.command('/photo_day', async (ctx: any) => {
   }).
   catch((error: any) => {
       ctx.reply(error)
-      console.log(error);
   });
+});
+
+bot.command('/mars', (ctx: any) => {
+  const fetchMarsPhoto = () => {
+    const intervalYear = Math.floor(Math.random() * (2022 - 2012)) + 2012;
+    const intervalMonth  = Math.floor(Math.random() * (12 - 1)) + 1;
+    const intervalDay  = Math.floor(Math.random() * (31 - 1)) + 1;
+    
+    const photoMarsUrl = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=${intervalYear}-${intervalMonth}-${intervalDay}&camera=MAST&api_key=` + process.env.API_KEY;
+
+    fetch(photoMarsUrl)
+      .then((response: any) => {
+        if (response.ok) {
+          response.json().then((data: any) => {
+            const randomPhoto = data.photos[Math.floor(Math.random() * data.photos.length) + 1]
+
+            if (data.photos[0]) {
+              ctx.replyWithPhoto(
+                {url: randomPhoto.img_src}, 
+                {caption: randomPhoto.earth_date}
+              )
+            } else {
+              fetchMarsPhoto()
+            }
+          });
+        }
+      }).
+      catch((error: any) => {
+          ctx.reply(error)
+      }
+    );
+  } 
+
+    fetchMarsPhoto();
 });
 
 bot.launch();
