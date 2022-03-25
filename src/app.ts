b59403329032be5e9067745a285376f5111be38b
Yuriy <yuriy.faizov@gmail.com>
2022-03-25 15:20:19 +0300

    Добавил firebase, база данных каждого чата с настройкой "Фото дня"

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,8 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, getDoc } = require('firebase-admin/firestore');
+
 var CronJob = require('cron').CronJob;
 
 const express = require('express')
@@ -11,15 +14,41 @@ var server=app.listen(port,function() {
 const token: string = process.env.BOT_TOKEN as string;
 const bot = new Telegraf(token)
 
+initializeApp();
+
+const db = getFirestore();
+db.settings({ ignoreUndefinedProperties: true })
+const chatsDb = db.collection('chats');
+
 const photoDay = require('./photoDay');
 const photoMars = require('./photoMars');
 
-let isStartPhotoDay = false
+type TChat = {
+  chatId: number;
+  type: string;
+  isStartPhotoDay?: boolean;
+}
 
-bot.start((ctx: any) => {
+bot.start( async (ctx: any) => {
+  const countUsers = await chatsDb.get().then((snap: any) => {
+    return snap.size
+  });
+
   ctx.reply(
-    'Здравствуйте ' + ctx.from.first_name + '! \n\nНа данный момент бот находится в разработке, на данный момент реализовано получаение фото дня и получение фотографий Марса с марсохода Curiosity \n\n\nСписок команд: \n\n/photo_day - Фото дня\n\n/mars - Случайная фотография из Марса'
+    'Здравствуйте ' + ctx.from.first_name + `! \n\nНа данный момент бот находится в разработке, на данный момент реализовано получаение фото дня и получение фотографий Марса с марсохода Curiosity \n\n\nСписок команд: \n\n/photo_day - Фото дня\n\n/mars - Случайная фотография из Марса \n\n\nКоличество пользователей: ${countUsers}`
   );
+
+  const chatRef = chatsDb.doc(`chat-${ctx.chat.id}`);
+  const dataChat = await chatRef.get();
+  const isStartPhotoDay = dataChat.data().isStartPhotoDay
+
+  const chat: TChat = {
+    chatId: ctx.chat.id,
+    type: ctx.chat.type,
+    isStartPhotoDay: isStartPhotoDay
+  };
+
+  await chatsDb.doc(`chat-${chat.chatId}`).set(chat);
 });
 
 bot.command('/photo_day', (ctx: any) => {
@@ -32,9 +61,29 @@ bot.command('/mars', async (ctx: any) => {
   photoMars.fetchMarsPhoto(ctx, startMessage.message_id);
 });
 
-bot.command('/photo_day_start', (ctx: any) => {
+bot.command('/photo_day_start', async (ctx: any) => {
+  const chatRef = chatsDb.doc(`chat-${ctx.chat.id}`);
+  const dataChat = await chatRef.get();
+  const isStartPhotoDay = dataChat.data().isStartPhotoDay
+
+  const chat: TChat = {
+    chatId: ctx.chat.id,
+    type: ctx.chat.type,
+    isStartPhotoDay: isStartPhotoDay
+  };
+
+  if (!dataChat.exists) {
+    await chatsDb.doc(`chat-${chat.chatId}`).set(chat)
+    chatsDb.doc(`chat-${ctx.chat.id}`).set({
+      isStartPhotoDay: true
+    }, { merge: true });
+  } else {
+    chatsDb.doc(`chat-${ctx.chat.id}`).set({
+      isStartPhotoDay: true
+    }, { merge: true });
+  }
+
   if (!isStartPhotoDay) {
-    isStartPhotoDay= true
     ctx.reply('Теперь фото дня будет приходить каждый день в 12:00 по МСК.')
 
     const job = new CronJob('00 12 * * *', function() {
@@ -47,9 +96,31 @@ bot.command('/photo_day_start', (ctx: any) => {
   }
 });
 
-bot.command('/photo_day_stop', (ctx: any) => {
+bot.command('/photo_day_stop', async (ctx: any) => {
+  const chatRef = chatsDb.doc(`chat-${ctx.chat.id}`);
+  const dataChat = await chatRef.get();
+  const isStartPhotoDay = dataChat.data().isStartPhotoDay
+
+  const chat: TChat = {
+    chatId: ctx.chat.id,
+    type: ctx.chat.type,
+    isStartPhotoDay: isStartPhotoDay
+  };
+
+
+  if (!dataChat.exists) {
+    await chatsDb.doc(`chat-${chat.chatId}`).set(chat)
+    chatsDb.doc(`chat-${ctx.chat.id}`).set({
+      isStartPhotoDay: false
+    }, { merge: true });
+  } else {
+    chatsDb.doc(`chat-${ctx.chat.id}`).set({
+      isStartPhotoDay: false
+    }, { merge: true });
+  }
+
+
   if (isStartPhotoDay) {
-    isStartPhotoDay = false
     ctx.reply('Теперь фото дня НЕ будет приходить каждый день.')
 
     const job = new CronJob('00 12 * * *', function() {
